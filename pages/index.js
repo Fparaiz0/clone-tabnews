4486353913e87aed59fe4588627863f4b87c9602
Felipe Paraizo de Oliveira <68759810+Fparaiz0@users.noreply.github.com>
2025-10-11 01:41:22 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Byanka, eu amo você. E quero desejar um Feliz Aniversário adiantado. Se você me ama, dá uma risadinha! 😎​</h1>
+  return (
+    <h1>
+      Byanka, eu amo você. E quero desejar um Feliz Aniversário adiantado. Se
+      você me ama, dá uma risadinha! 😎​
+    </h1>
+  );
 }
 
-export default Home; 
+export default Home;
