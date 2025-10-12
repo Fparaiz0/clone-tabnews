9880aa519805aad2e952cc112600d39df2bb49e3
Felipe Paraizo de Oliveira <68759810+Fparaiz0@users.noreply.github.com>
2025-10-12 18:49:19 +0000

    adiciona pagina em construção

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
+import "./Home.css";
+
 function Home() {
   return (
-    <h1>
-      Byanka, eu amo você. E quero desejar um Feliz Aniversário adiantado. Se
-      você me ama, dá uma risadinha! 😎​
-    </h1>
+    <div className="container">
+      <h1>🚧 Portfólio em Construção 🚧</h1>
+      <p>
+        Oi, eu sou <span className="name">Felipe Paraizo</span>! <br />
+        Este será o meu portfólio — em breve, cheio de projetos incríveis.
+      </p>
+      <div className="loading-bar"></div>
+      <p className="footer">Volte em breve 😄</p>
+    </div>
   );
 }
 
