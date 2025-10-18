3953775eaff37e99986620cd3f53e78d6735fc0c
Felipe Paraizo de Oliveira <68759810+Fparaiz0@users.noreply.github.com>
2025-10-18 10:23:39 +0000

    database `version` + red, green and refactor of tdd

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,14 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Minha primeira API" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SELECT version();");
+  console.log(databaseVersionResult);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+  });
 }
 
 export default status;
